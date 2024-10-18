c4ae7dd620bff1d5dd136f88dd750495176e19b0
Sam <sdwill@uvm.edu>
2024-10-18 13:29:30 -0400

    First commit (ignoring .idea stuff)

---
+++ captcha.py
@@ -0,0 +1,15 @@
+# load needed libraries.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# sklearn utilities
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+# sklearn models
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import tensorflow as tf
+

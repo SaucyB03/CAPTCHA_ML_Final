df4918fe882b9de57a8d42118cf7808121246271
SaucyB03 <105938116+SaucyB03@users.noreply.github.com>
2024-10-22 18:22:53 -0400

    Got model to train and test on every character using a T/F y_train for if it was a chosen character or not

--- captcha.py
+++ captcha.py
@@ -45,24 +45,30 @@ imagew = [
     [110, 135]
     ]
 
+chosen_chars = ['2','d','g']
+
 img_num = 0
 
 #Converts images to greyscale
 for img in range(len(image_files)):
     temp = image_files[img][8:13]
-    names.append(temp[0])
-    new_img.append([])
+
     for a in range(5):
         new_img.append([])
-        img_num += 1
+        if temp[a] in chosen_chars:
+            names.append(True)
+        else:
+            names.append(False)
         for w in range(imagew[a][0], imagew[a][1]):
             for h in range(images.shape[1]):
                 if images[img][h][w][0] < IMG_THRESHHOLD/255:
                     new_img[img_num].append(images[img][h][w][0])
                 else:
                     new_img[img_num].append(1.0)
-    print("Image ", img_num+1, "/", len(images)*5, " successfully converted to greyscale")
-    img_num += 1
+
+        img_num += 1
+        print("Image ", img_num, "/", len(images)*5, " successfully converted to greyscale")
+    # img_num += 1
 
 new_img = numpy.array(new_img)
 print(new_img.shape)
@@ -79,7 +85,7 @@ for i, ax in enumerate(axes.flat):
         start_idx = x * 50 * (imagew[x][1] - imagew[x][0])
         end_idx = (x + 1) * 50 * (imagew[x][1] - imagew[x][0])
         # Reshape only the appropriate section of new_img[i]
-        reshaped_image = np.reshape(new_img[i], (50, imagew[x][1] - imagew[x][0]))
+        reshaped_image = np.reshape(new_img[i], (imagew[x][1] - imagew[x][0], 50)).T
         ax.imshow(reshaped_image, cmap='gray', interpolation="nearest")  # Display each image section in grayscale
 
 # Display the grid of images
@@ -101,14 +107,14 @@ y_test = np.array(y_test)
 print("Training x:", x_train.shape,"y:", y_train.shape)
 print("Testing x:", x_test.shape,"y:", y_test.shape)
 
-scores = []
-for i in range(1, x_train.shape[0]):
-    #add your code here.
-    treeI = DecisionTreeClassifier(max_features = i)
-    treeI.fit(x_train, y_train)
-    scores.append(treeI.score(x_test, y_test))
-
-plt.plot(scores)
+# scores = []
+# for i in range(1, x_train.shape[0]):
+#     #add your code here.
+#     treeI = DecisionTreeClassifier(max_features = i)
+#     treeI.fit(x_train, y_train)
+#     scores.append(treeI.score(x_test, y_test))
+#
+# plt.plot(scores)
 
 tree = DecisionTreeClassifier()
 
